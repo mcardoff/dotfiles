ac3f75bac220dc8bc891fff943eb25d623f7fcc2
Michael Cardiff <mcardiff0321@gmail.com>
2021-07-05 10:05:20 -0500

    Added projectile and deprecated useless packages in emacs config

--- regmacs/.emacs.d/elpa/micgoline-readme.txt
+++
@@ -1,2 +0,0 @@
-micgoline is a modular mode-line library built on the powerline package,
-designed to make it easy to build beautiful mode-lines.
--- regmacs/.emacs.d/transient/history.el
+++ regmacs/.emacs.d/transient/history.el
@@ -1,6 +1,7 @@
 ((magit-commit nil
 	       ("--all"))
  (magit-merge nil)
+ (magit-pull nil)
  (magit-push nil
 	     ("--force")
 	     ("--force-with-lease"))
