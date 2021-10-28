6155ebf65c7ee161cf486705436dbbb7d4e43eb6
Michael Cardiff <mcardiff0321@gmail.com>
2021-10-28 13:10:10 -0500

    [bin] Changing tex package install script

--- .bin/texpkg.sh
+++ .bin/texpkg.sh
@@ -1,32 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
 # Directory to place it is
 # Name of directory should be first argument, run with sudo
-DIR=/usr/share/texmf/tex/latex/$1
+DIR=/usr/share/texmf/tex/latex/
+cd /home/mcard/Downloads/package_install
 
-if [ ! -d $DIR ]; then
-    sudo mkdir $DIR
-fi
-
-if [ ! -d /home/mcard/Downloads/$1 ]; then
-    unzip /home/mcard/Downloads/$1 -d /home/mcard/Downloads/
-fi
-
-cd /home/mcard/Downloads/$1
-
-if [ ! -f /home/mcard/Downloads/$1/$1.sty ]; then
-    if [ -f /home/mcard/Downloads/$1/$1.ins ]; then
-	# we have to compile
-	latex /home/mcard/Downloads/$1/$1.ins
-    elif [ -f /home/mcard/Downloads/$1/$1.dtx ]; then
-	tex /home/mcard/Downloads/$1/$1.dtx
+for z in *.zip; do
+    # echo $z
+    if [ ! -d $DIR/$z ]; then
+	sudo mkdir $DIR/${z%.*}
+    fi
+    # unzip
+    unzip $z
+    if [ ! -f ./${z%.*}.sty ]; then # .sty not available, compile instead
+	if [ -f ./${z%.*}/${z%.*}.ins ]; then
+	    latex ./${z%.*}/${z%.*}.ins
+	elif [ -f ./${z%.*}/${z%.*}.dtx ]; then
+	    tex ./${z%.*}/${z%.*}.dtx
     fi
 fi
+done
 
-# now name.sty exists, copy it to texpath
-sudo cp /home/mcard/Downloads/$1/$1.sty $DIR
 
-# redo texhash
-sudo texhash
+# cd /home/mcard/Downloads/$1
+
+# if [ ! -f /home/mcard/Downloads/$1/$1.sty ]; then
+    # if [ -f /home/mcard/Downloads/$1/$1.ins ]; then
+	# we have to compile
+	# latex /home/mcard/Downloads/$1/$1.ins
+    # elif [ -f /home/mcard/Downloads/$1/$1.dtx ]; then
+	# tex /home/mcard/Downloads/$1/$1.dtx
+    # fi
+# fi
+
+# # now name.sty exists, copy it to texpath
+# sudo cp /home/mcard/Downloads/$1/$1.sty $DIR
+
+# # redo texhash
+# sudo texhash
 
 
