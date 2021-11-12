6148bfd5df85760885b0a95cb9385aac3450fc75
Michael Cardiff <mcardiff0321@gmail.com>
2021-11-12 16:17:42 -0600

    [bin] changed ranger script so it is one line

--- .bin/openers/ranger.sh
+++ .bin/openers/ranger.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-pid=$(pgrep ranger)
 
-# echo $pid
-
-if [ -n "$pid" ] ; then
-    echo $pid
-else
-    alacritty -t Ranger -e ranger
-fi
-		     
+[ -z "$(pgrep ranger)" ] && alacritty -t Ranger -e ranger		     
