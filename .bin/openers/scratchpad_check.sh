e7a318c67961975a48bbdaca7356054c0ac00206
Michael Cardiff <mcardiff0321@gmail.com>
2021-11-12 16:18:41 -0600

    [bin/i3] added script to check if i3 scratchpads were running

---
+++ .bin/openers/scratchpad_check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Logic for dropdown term and emacs w/ server
+# need it to be bigger than one line, pipe through wc -l, as ps will list 
+# if it equals one, it is just the grep process, we do not need to run again
+
+# Ranger 
+[ -z "$(pgrep ranger)" ] && (~/.cargo/bin/alacritty -t Ranger -e ranger &)
+
+# dropdown term
+[ $(ps -ef | grep dropterm | wc -l) -eq 1 ] &&
+    ~/.cargo/bin/alacritty -t dropterm &
+
+# emacs running server
+[ $(ps -ef | grep notepad | wc -l) -eq 1 ] &&
+    emacs -T notepad --eval="(unless (boundp 'server-process) (server-start))" &
+
+# feh with schedule adds 2 instead of 1, but the basic is the same
+[ $(ps -ef | grep Schedule | wc -l) -eq 1 ] &&
+    feh ~/Pictures/schedule.png --title "Schedule" &
