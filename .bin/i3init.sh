6d6b7442dc0b3a4470af765f5cf883f0deb386f5
Michael Cardiff <mcardiff0321@gmail.com>
2021-11-12 16:17:21 -0600

    [i3] changed init script so it actually works

--- .bin/i3init.sh
+++ .bin/i3init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-~/.bin/kill-if-run.sh compton
-~/.bin/kill-if-run.sh nitrogen restore
-~/.bin/kill-if-run.sh lxappearance
+~/.bin/kill-if-run.sh compton &
+~/.bin/kill-if-run.sh nitrogen restore &
+~/.bin/kill-if-run.sh lxappearance &
