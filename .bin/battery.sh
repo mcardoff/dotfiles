da515f46c5608dd072b4207166b40d1e44c3cc43
Michael Cardiff <mcardiff0321@gmail.com>
2021-11-30 14:19:47 -0600

    [battery] improved battery script

--- .bin/battery.sh
+++ .bin/battery.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 BAT="/sys/class/power_supply/BAT0/"
-
 STAT=$(cat $BAT/status)
 PER=$(( 100 * $(cat $BAT/energy_now) / $(cat $BAT/energy_full) ))
 num=$(( PER * 8 / 100 ))
-bolt="idle"
 
+bolt="idle"
 pat='/home/mcard/.config/xmonad/xpm/batt/'
 
-[ "$STAT" = "Unknown" ] && bolt="charging"
 
-color=$(cat $pat$bolt"_"$num.xpm | grep -m 1 "\". c #" | sed 's/\"//g' | sed 's/. c //' | sed 's/,//')
+{ [ "$STAT" = "Charging" ] || [ "$STAT" = "Unknown" ] ;} && bolt="charging"
 
-# echo $color
-bar="<icon=batt/"$bolt"_"$num".xpm/><fc=$color>"
+color=$(grep -m 1 "\". c #" $pat$bolt$num.xpm | sed 's/\"//g' | sed 's/. c //' | sed 's/,//')
 
-echo "$bar $PER%</fc>" 
+echo "<icon=batt/$bolt$num.xpm/><fc=$color>" "$PER%</fc>" 
+
