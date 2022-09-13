7ae605c7c8004db3ab81abb5d0dde749b1a8bca9
Michael Cardiff <mcardiff0321@gmail.com>
2022-09-12 20:29:03 -0500

    Changed book directories

--- .bin/books.sh
+++ .bin/books.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PAT="/home/mcard/school/Books/"
+PAT="/home/mcard/school/misc/Books/"
 OUTPUT=$(find $PAT -type f -iname '*.pdf' | awk -F '/' '{print $NF}' | dmenu)
 
 if [ -n "$OUTPUT" ]; then
