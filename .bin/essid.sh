7ce8a86d77384a503749209fee6e655702f07512
Michael Cardiff <mcardiff0321@gmail.com>
2023-07-17 15:22:19 -0400

    Remove deprecated/unused scripts

--- .bin/essid.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-essid=`iwconfig wlp2s0 | awk -F '"' '/ESSID/ {print $2}'`
-echo $essid
--- .bin/getsink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
-
-echo $SINK
-
-exit 0
