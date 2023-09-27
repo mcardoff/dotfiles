0e32ab7d1a8a8b35cc8822e3909e7d1e024f5414
Michael Cardiff <mcardiff0321@gmail.com>
2023-09-27 16:56:17 -0400

    [bin] add script to find next lecture num based on hw num

---
+++ .bin/find_next_lec.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#INPUT FORMAT:
+# $1 = SEMESTER
+# $2 = CLASS
+
+BIGGEST=$(dir -1 /home/mcard/school/$1/$2/Lab_L*.tex 2> /dev/null | sed 's/^.*Lecture_//' | sed 's/.tex//g')
+
+[[ -z "$BIGGEST" ]] && BIGGEST=0
+
+NEXTNUM=$((10#$BIGGEST + 1))
+FINAL="$NEXTNUM"
+
+[[ $NEXTNUM -lt 10 ]] && FINAL="0$NEXTNUM"
+
+# Print next file name
+printf "/home/mcard/school/$1/$2/Lab_Lecture_$FINAL.tex"
