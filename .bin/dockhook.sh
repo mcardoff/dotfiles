a3e22ff44b22d3c9b919e63c616db713600e9dda
Michael Cardiff <mcardiff0321@gmail.com>
2023-10-20 23:52:22 -0400

    Work on Dock hood

--- .bin/dockhook.sh
+++ .bin/dockhook.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-isconnected=$(lsusb | grep "ThinkPad Dock Hub" | head -n 1)
-[[ -z "$isconnected" ]] && echo "Not connected"
-# echo $isconnected
+xrandr --output DP-2-3 --right-of eDP-1 --mode 1920x1080 ||
+    xrandr --output DP-1-3 --right-of eDP-1 --mode 1920x1080
+source /home/mcard/.local/scripts/cisbg.sh
