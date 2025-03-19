7e011347fa1986bd218da5c4ca42929183072d1f
Michael Cardiff <mcardiff@brandeis.edu>
2025-03-18 17:52:37 -0700

    Add defuns

---
+++ .config/emacs/convenient-defuns.el
@@ -0,0 +1,19 @@
+(defun dotemacs ()
+  "Opens init.el"
+  (interactive)
+  (find-file user-init-file))
+
+(defun initorg ()
+  "Opens EmacsInit.org"
+  (interactive)
+  (find-file (format "%s%s" user-emacs-directory "EmacsInit.org")))
+
+(defun projectorg ()
+  "Opens Current_Projects.org"
+  (interactive)
+  (find-file "~/Org/Agenda/Current_Projects.org"))
+
+(defun agendafile ()
+  "open the latest modified org-agenda file"
+  (interactive)
+  (find-file (shell-command-to-string "/home/mcard/.local/scripts/latestorg.sh")))
